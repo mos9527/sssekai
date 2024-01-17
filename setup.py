f88684ac7b86c73e5e5b7884f90d5a77bf9f38a4
mos9527 <31397301+mos9527@users.noreply.github.com>
2024-01-17 17:04:53 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys,os
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 import setuptools, sssekai
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
