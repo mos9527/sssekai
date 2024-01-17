2e34177bfccc288cce1ca492a4a563861513a1f0
greats3an <greats3an@gmail.com>
2024-01-17 20:29:25 +0800

    Cleaning up

--- sssekai.test.py
+++ sssekai.test.py
@@ -1,12 +1,14 @@
 from sssekai.unity.AnimationClip import read_animation
 from sssekai.unity.AssetBundle import load_assetbundle
+import sys
+sys.path.insert(0,r'C:\Users\Huang')
 from sssekai_blender_io.blender.asset import search_env_animations, search_env_meshes
 
 with open(r"F:\Sekai\live_pv\timeline\0008\camera",'rb') as f:
     env = load_assetbundle(f)
     anims = search_env_animations(env)
     for anim in anims:
-        if 'main_cam' in anim.name:
+        if 'depth_' in anim.name:
             data = read_animation(anim)
             pass
         pass
