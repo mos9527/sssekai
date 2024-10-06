c0008bfbb6c9d4348d1d12fd1a7ca000bcf39ef1
greats3an <greats3an@gmail.com>
2024-10-06 23:26:48 +0800

    Bump UnityPy required version to 1.20.4

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
     install_requires=[
         "msgpack",
         "pycryptodome",
-        "unitypy >= 1.10.14",
+        "unitypy >= 1.20.4",
         "wannacri",
         "python-json-logger",
         "tqdm",
