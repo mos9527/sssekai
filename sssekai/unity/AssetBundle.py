79292ad5981f3c3b099001aca1db80ef52124028
mos9527 <greats3an@gmail.com>
2024-11-21 20:38:58 +0800

    BinaryIO -> BytesIO

--- sssekai/unity/AssetBundle.py
+++ sssekai/unity/AssetBundle.py
@@ -1,14 +1,14 @@
-from typing import BinaryIO
+from io import BytesIO
 from sssekai.crypto.AssetBundle import decrypt_iter
 
 from . import UnityPy, sssekai_get_unity_version
 from UnityPy import Environment
 
 
-def load_assetbundle(file: BinaryIO) -> Environment:
+def load_assetbundle(file: BytesIO) -> Environment:
     UnityPy.config.FALLBACK_VERSION_WARNED = True
     UnityPy.config.FALLBACK_UNITY_VERSION = sssekai_get_unity_version()
-    stream = BinaryIO()
+    stream = BytesIO()
     for block in decrypt_iter(lambda nbytes: file.read(nbytes)):
         stream.write(block)
     return UnityPy.load(stream)
