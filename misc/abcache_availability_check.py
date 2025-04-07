2370dd65b7138b89cef49ac884ad1d684517ea72
mos9527 <greats3an@gmail.com>
2025-04-07 12:17:00 +0800

    Update apphash heuristic

--- misc/abcache_availability_check.py
+++ misc/abcache_availability_check.py
@@ -4,12 +4,14 @@ from sssekai.abcache import AbCache, AbCacheConfig
 coloredlogs.install(level=0)
 logger = logging.getLogger("abcache-test")
 
-config = AbCacheConfig(
-    app_region="cn",
-    app_version="3.4.1",
-    app_hash="a3015fe8-785f-27e1-fb8b-546a23c82c1f",
-    app_platform="android",
+cache = AbCache(
+    AbCacheConfig(
+        app_region="cn",
+        app_version="3.4.1",
+        ab_version="3.4.0",
+        app_hash="a3015fe8-785f-27e1-fb8b-546a23c82c1f",
+        app_platform="android",
+    )
 )
-cache = AbCache(config)
 cache.update()
 pass
--- sssekai/entrypoint/apphash.py
+++ sssekai/entrypoint/apphash.py
@@ -6,18 +6,18 @@ import sys
 
 from io import BytesIO
 
-import UnityPy.enums
-import UnityPy.enums.ClassIDType
-from UnityPy.classes import MonoBehaviour
+from UnityPy.enums import ClassIDType
+from UnityPy.helpers.Tpk import get_typetree_node
 from sssekai.unity.AssetBundle import load_assetbundle
 from tqdm import tqdm
 
 REGION_MAP = {
-    "com.sega.pjsekai": "jp",
-    "com.sega.ColorfulStage.en": "en",
-    "com.hermes.mk.asia": "tw",
-    "com.pjsekai.kr": "kr",
+    # NOTE: Order is used to determine the region
     "com.hermes.mk": "cn",
+    "com.hermes.mk.asia": "tw",
+    "com.sega.ColorfulStage.en": "en",
+    "com.sega.pjsekai": "jp",
+    "com.pjsekai.kr": "kr",
 }
 logger = logging.getLogger("apphash")
 
@@ -35,9 +35,30 @@ def enum_package(zip_file):
             yield zipfile.ZipFile(zip_file.open(f))
 
 
+def dump_axml_stringpool(f: BytesIO):
+    read_int = lambda nbytes: int.from_bytes(f.read(nbytes), "little")
+    f.seek(8)
+    hdr_type, hdr_size, size = read_int(2), read_int(2), read_int(4)
+    n_strings = read_int(4)
+    unk = read_int(4)
+    flags = read_int(4)
+    is_utf8 = flags & (1 << 8)
+    string_offset = read_int(4) + 8
+    unk = read_int(4)
+    offsets = [read_int(4) for _ in range(n_strings)]
+    for offset in offsets:
+        f.seek(string_offset + offset)
+        string_len = read_int(2)
+        if is_utf8:
+            string = f.read(string_len).decode("utf-8")
+        else:
+            string = f.read(string_len * 2).decode("utf-16-le")
+        yield string
+
+
 def main_apphash(args):
     env = UnityPy.Environment()
-    app_package = "unknown"
+    app_package = None
     app_version = "unknown"
     app_hash = "unknown"
 
@@ -61,6 +82,20 @@ def main_apphash(args):
         else:
             src = open(args.apk_src, "rb")
         with zipfile.ZipFile(src, "r") as zip_ref:
+            manifests = [
+                manifest
+                for package in enum_package(zip_ref)
+                for manifest in enum_candidates(
+                    package, lambda fn: fn == "AndroidManifest.xml"
+                )
+            ]
+            manifest = manifests[0][1]
+            manifest_strings = set(dump_axml_stringpool(manifest))
+            # Heur: Reverse lookup the package name from the manifest strings
+            for ky in REGION_MAP:
+                if ky in manifest_strings:
+                    app_package = ky
+                    break
             candidates = [
                 candidate
                 for package in enum_package(zip_ref)
@@ -70,7 +105,7 @@ def main_apphash(args):
                     in {
                         "6350e2ec327334c8a9b7f494f344a761",  # PJSK Android
                         "c726e51b6fe37463685916a1687158dd",  # PJSK iOS
-                        "data.unity3d",  # TW,KR (ByteDance)
+                        "data.unity3d",  # TW,KR,CN (ByteDance)
                     },
                 )
             ]
@@ -84,15 +119,20 @@ def main_apphash(args):
     from sssekai.generated import TYPETREE_DEFS
     from sssekai.generated.Sekai import PlayerSettingConfig
 
+    ans = None
     for pobj in env.objects:
         if pobj.type == UnityPy.enums.ClassIDType.MonoBehaviour:
-            obj = pobj.read(check_read=False)
-            obj: MonoBehaviour
+            # Only read base MonoBehaviour class to speed things up (a lot)
+            # XXX: pboj.peek_name() fails here.
+            pname = pobj.read_typetree(
+                get_typetree_node(ClassIDType.MonoBehaviour, pobj.version),
+                check_read=False,
+            )["m_Name"]
             # Can't use UTTCGen_Reread because we may have no access to the Script PPtr asset
             # Object name seems to be a good enough heuristic
             for name in {"production_android", "production_ios"}:
-                if obj.m_Name == name:
-                    tt = obj.object_reader.read_typetree(
+                if pname == name:
+                    tt = pobj.read_typetree(
                         TYPETREE_DEFS["Sekai.PlayerSettingConfig"], check_read=False
                     )
                     config = PlayerSettingConfig(**tt)
@@ -112,22 +152,38 @@ def main_apphash(args):
                         config.clientDataRevision,
                     )
                     app_hash = config.clientAppHash
-                    app_package = config.bundleIdentifier
-                    region = region = REGION_MAP.get(app_package, "unknown")
+                    # XXX: CN build has the same package name as the JP build??
+                    # We need another heuristic to determine the package name
+                    # This is previously done by setting app_package
+                    package_heur = app_package or config.bundleIdentifier
+                    region = region = REGION_MAP.get(package_heur, "unknown")
                     print(
                         f"Found {config.productName} at {config.m_Name}",
                         f"  Memo: {config.memo}",
-                        f"  Package: {config.bundleIdentifier}",
+                        f"  Package: {config.bundleIdentifier} (actually assumed as {package_heur})",
                         f"  AppHash (app_hash):     {config.clientAppHash}",
-                        f"  Region  (app_region):   {region}",
+                        f"  Region  (app_region):   {region} (determined by {package_heur})",
                         f"  Version (app_version):  {app_version}",
                         f"  Version (ab_version):   {ab_version}",
-                        f"      (NOTE: Set this ONLY with ROW (TW/KR/CN) versions!)",
                         f"  Bundle Version: {config.bundleVersion}",
                         f"  Data Version:   {data_version}",
                         f"  Version Suffix: {config.clientVersionSuffix}" "",
                         sep="\n",
                         file=sys.stderr,
                     )
+                    # Only keep the Android one
+                    if name == "production_android":
+                        ans = f"""
+# Auto-Generated by sssekai/entrypoint/apphash.py
+# CLI Usage:
+# sssekai abcache --app-platform android --app-region {region} --app-version {app_version} --app-appHash {app_hash} --app-abVersion {ab_version}
+# Python Usage:
 
-    print(app_version, region, app_hash)
+AbCacheConfig(
+    app_region="{region}",
+    app_version="{app_version}",
+    ab_version="{ab_version}",
+    app_hash="{app_hash}",
+    app_platform="android"
+)"""
+    print(ans)
